8251b854f52bf4e70b97cd35426033114daefb27
vinothkumar1590 <a.vinothkumar1590@hotmail.com>
2020-04-07 19:22:57 +0000

    added function in script

--- helloworld.sh
+++ helloworld.sh
@@ -1,2 +1,16 @@
 #!/bin/bash -e
 echo "Hello World"
+function check_os {
+	check=$(uname -mrs | cut -d ' ' -f1)
+	if [ $check == Linux ]
+	then
+		echo " OS is Linux Variant"
+	elif [ $check == Darwin ]
+	then
+	       echo "OS is Mac OS Variant"
+        else
+ 		echo "No Result"
+	fi
+}
+check_os
+
