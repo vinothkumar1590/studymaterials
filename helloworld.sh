4b850faeccdfd4058d53a2dfc792bae25de1a74c
vinothkumar1590 <57338520+vinothkumar1590@users.noreply.github.com>
2020-04-09 20:52:24 +0530

    updated check_os

--- helloworld.sh
+++ helloworld.sh
@@ -1,2 +1,13 @@
 #!/bin/bash -e
 echo "Hello World"
+function check_os {
+  OS=$(uname -mrs | cut -d ' ' -f1)
+  if [ $check == Linux ]
+  then
+  echo "OS is Linux Variant"
+  elif [ $check == Darwin ]
+  then
+  echo "OS is Darwin Variant"
+  fi
+  }
+  check_os
